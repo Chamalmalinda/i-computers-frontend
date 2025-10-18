dd140e96d9541751d57ebbcb77f2804f955355ef
Chamal Malinda <chamalmalinda00@gmail.com>
2025-10-18 12:47:14 +0530

    Day 10✌️

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import ProductCard from './components/productCard'
+
+function App() {
+
+
+  return (
+    <>
+
+        <ProductCard name="Laptop"price="100,00.00"image="https://picsum.photos/id/1/200/300"/>
+        <ProductCard name="Phone"price="90,00.00"image="https://picsum.photos/id/3/200/300"/>
+        <ProductCard name="Watch"price="20,00.00"image="https://picsum.photos/id/5/200/300"/>
+    </>
+  )
+}
+
+export default App
